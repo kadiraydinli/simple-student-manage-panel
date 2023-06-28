4a4cc6f1b373185c121bc455e5a9aedbf3cea6f5
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-06-29 01:14:10 +0300

    create Header component

---
+++ components/Header.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { MouseEvent } from "react";
+import { usePathname, useRouter } from "next/navigation";
+
+import Icon from "@/assets/icons";
+
+const Header = () => {
+	const pathname = usePathname();
+	const router = useRouter();
+
+	const isMainPage = pathname === "/";
+
+	const onBack = (e: MouseEvent<HTMLElement>) => {
+		e.preventDefault();
+		router.back();
+	};
+
+	const onNotifications = () => {};
+
+	return (
+		<div className="h-fit py-5 px-8 flex flex-row items-center justify-between">
+			<button
+				className="text-[--icon] hover:text-black cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
+				onClick={onBack}
+				disabled={isMainPage}
+			>
+				<Icon.CircleArrowLeft />
+			</button>
+			<button
+				className="text-[--icon] hover:text-black cursor-pointer"
+				onClick={onNotifications}
+			>
+				<Icon.Bell />
+			</button>
+		</div>
+	);
+};
+
+export default Header;
