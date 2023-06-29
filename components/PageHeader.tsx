1a9d98f02a9fa576578613f39e83ff36af3ba3f0
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-06-29 21:49:50 +0300

    add divider to PageHeader

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -15,18 +15,22 @@ const PageHeader: React.FC<PageHeaderProps> = ({
 	buttonProps,
 }) => {
 	return (
-		<div className="h-[68px] px-8 py-3 flex flex-row items-center justify-between">
-			<h1 className="flex-1 text-[22px] font-bold text-black">{title}</h1>
-			{(searchInputProps || buttonProps) && (
-				<div className="gap-6 flex flex-auto flex-row items-center justify-end">
-					{searchInputProps && (
-						<SearchInput placeholder="Search..." {...searchInputProps} />
-					)}
-					{buttonProps && (
-						<Button className="w-[200px]" {...buttonProps}></Button>
-					)}
-				</div>
-			)}
+		<div className="h-[68px] px-8 pt-3">
+			<div className="flex flex-row w-full items-center justify-between pb-3">
+				<h1 className="flex-1 text-[22px] font-bold text-black">{title}</h1>
+				{(searchInputProps || buttonProps) && (
+					<div className="gap-6 flex flex-auto flex-row items-center justify-end">
+						{searchInputProps && (
+							<SearchInput placeholder="Search..." {...searchInputProps} />
+						)}
+						{buttonProps && (
+							<Button className="w-[200px]" {...buttonProps}></Button>
+						)}
+					</div>
+				)}
+			</div>
+
+			<hr className="border-[--border]" />
 		</div>
 	);
 };
