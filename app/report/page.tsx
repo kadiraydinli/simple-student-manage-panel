69cdcba5c0245e915ef1b13d17624d9e9a232ecd
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-06-29 19:14:25 +0300

    add new report page

---
+++ app/report/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Header from "@/components/Header";
+import PageHeader from "@/components/PageHeader";
+
+const Report = () => {
+	return (
+		<div className="w-full h-full bg-[--background]">
+			<Header />
+			<PageHeader title="Report" />
+		</div>
+	);
+};
+
+export default Report;
