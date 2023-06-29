42c3a78a5244ea5e1575040bbc1a7abbc55e9363
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-06-29 19:15:18 +0300

    add new settings page

---
+++ app/settings/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Header from "@/components/Header";
+import PageHeader from "@/components/PageHeader";
+
+const Settings = () => {
+	return (
+		<div className="w-full h-full bg-[--background]">
+			<Header />
+			<PageHeader title="Settings" />
+		</div>
+	);
+};
+
+export default Settings;
