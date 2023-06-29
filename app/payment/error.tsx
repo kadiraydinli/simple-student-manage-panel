1be9a94de1acfb8b2ddf824970cbf6945ab49b89
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-06-29 19:13:41 +0300

    add new payment page

---
+++ app/payment/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Error = () => {
+	return (
+		<div className="w-full h-full flex justify-center items-center bg-gradient-to-r from-[--primary] to-[--secondary]">
+			<h1 className="font-bold text-3xl">Something went wrong!</h1>
+		</div>
+	);
+};
+
+export default Error;
---
+++ app/payment/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Header from "@/components/Header";
+import PageHeader from "@/components/PageHeader";
+
+const Payment = () => {
+	return (
+		<div className="w-full h-full bg-[--background]">
+			<Header />
+			<PageHeader title="Payment" />
+		</div>
+	);
+};
+
+export default Payment;
