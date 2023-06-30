8bdde5c0f086f3c40e2eb42c7a818601afcc7955
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-06-30 20:55:44 +0300

    fix Sidebar style

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -59,7 +59,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
 
 	return (
 		<div className="flex h-screen">
-			<div className="w-[270px] relative bg-[#F2EAE1] flex flex-col items-center pt-5 pb-8">
+			<div className="w-[270px] relative bg-[#F2EAE1] flex flex-col items-center pt-5 pb-8 overflow-y-auto">
 				<div className="flex flex-col items-center">
 					<div className="flex flex-row mb-20">
 						<div className="border-l-4 border-[--secondary] mr-1.5"></div>
