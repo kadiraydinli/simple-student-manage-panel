51c57880667f3e7cc4e27aabe9bf11f578920b03
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-07-03 00:10:46 +0300

    PageHeader refactor

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -5,24 +5,22 @@ import SearchInput from "@/components/SearchInput";
 
 interface PageHeaderProps {
 	title: string;
-	searchInputProps?: React.InputHTMLAttributes<HTMLInputElement>;
+	shownSearchInput?: boolean;
 	buttonProps?: React.ButtonHTMLAttributes<HTMLButtonElement>;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = ({
 	title,
-	searchInputProps,
+	shownSearchInput,
 	buttonProps,
 }) => {
 	return (
 		<div className="h-[68px] px-8 pt-3">
 			<div className="flex flex-row w-full items-center justify-between pb-3">
 				<h1 className="flex-1 text-[22px] font-bold text-black">{title}</h1>
-				{(searchInputProps || buttonProps) && (
+				{(shownSearchInput || buttonProps) && (
 					<div className="gap-6 flex flex-auto flex-row items-center justify-end">
-						{searchInputProps && (
-							<SearchInput placeholder="Search..." {...searchInputProps} />
-						)}
+						{shownSearchInput && <SearchInput />}
 						{buttonProps && (
 							<Button className="w-[200px]" {...buttonProps}></Button>
 						)}
