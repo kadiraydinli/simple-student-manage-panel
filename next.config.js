fd69af165a6b9575c5a36171eb394af7d6134220
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-07-02 20:26:41 +0300

    add domain to next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["picsum.photos"],
+		domains: ["picsum.photos", "robohash.org"],
 	},
 	webpack(config) {
 		// Grab the existing rule that handles SVG imports
