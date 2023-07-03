0b4f3c53999c744602a1508dfce4ec2bd8054e2e
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-07-03 17:18:02 +0300

    add empty state to students page

--- app/students/page.tsx
+++ app/students/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import { useRouter } from "next/navigation";
 
 import Header from "@/components/Header";
 import PageHeader from "@/components/PageHeader";
 import useStudents from "@/hooks/useStudents";
+import EmptyState from "@/components/EmptyState";
+import LoadingState from "@/components/LoadingState";
 
 import List from "./components/List";
 
@@ -19,7 +21,7 @@ interface StudentsProps {
 
 const Students: React.FC<StudentsProps> = ({ searchParams }) => {
 	const router = useRouter();
-	const { students, getStudents, total } = useStudents();
+	const { students, getStudents, total, loading } = useStudents();
 
 	const onAddStudent = () => {
 		router.push("/students/add");
@@ -39,6 +41,32 @@ const Students: React.FC<StudentsProps> = ({ searchParams }) => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
+	const content = useMemo(() => {
+		if (loading) {
+			return <LoadingState />;
+		}
+
+		if (searchParams.search && students.length === 0) {
+			return (
+				<EmptyState
+					title="Students Not Found!"
+					description="Try searching again with a different word."
+				/>
+			);
+		}
+
+		if (students.length === 0) {
+			return (
+				<EmptyState
+					title="There are no students!"
+					description="You can add new students."
+				/>
+			);
+		}
+
+		return <List students={students} total={total} />;
+	}, [loading, searchParams.search, students, total]);
+
 	return (
 		<div className="h-full bg-[--background] flex flex-col">
 			<Header />
@@ -48,7 +76,7 @@ const Students: React.FC<StudentsProps> = ({ searchParams }) => {
 				buttonProps={{ children: "ADD NEW STUDENT", onClick: onAddStudent }}
 			/>
 			<div className="flex flex-1 py-5 px-8 overflow-hidden overflow-y-hidden">
-				<List students={students} total={total} />
+				{content}
 			</div>
 		</div>
 	);
