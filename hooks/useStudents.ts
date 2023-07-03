fcd8e0d2cfba982903645abd39a62d95b8bf4fa2
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-07-03 17:13:41 +0300

    add loading to useStudents

--- hooks/useStudents.ts
+++ hooks/useStudents.ts
@@ -5,9 +5,11 @@ import { DataType, PER_PAGE_SIZES, UserType } from "@/types";
 type GetStudentType = { page?: number, size?: number, search?: string }
 
 const useStudents = () => {
+    const [loading, setLoading] = useState<boolean>(true);
     const [data, setData] = useState<DataType | null>(null);
 
     const getStudents = async ({ size = PER_PAGE_SIZES[0], page = 0, search }: GetStudentType) => {
+        setLoading(true);
 
         let url = "/api/students";
 
@@ -25,17 +27,23 @@ const useStudents = () => {
 
         const res = await fetch(url, { method: "GET" });
         const result: DataType = await res.json();
+
         setData(result);
+        setLoading(false);
     };
 
     const deleteStudent = async (id: number) => {
+        setLoading(true);
+
         const res = await fetch(`/api/students/${id}`, { method: "DELETE" });
         const result: UserType = await res.json();
 
+        setLoading(false);
         return result && result.isDeleted;
     };
 
     return {
+        loading,
         getStudents,
         deleteStudent,
         students: data?.users || [],
