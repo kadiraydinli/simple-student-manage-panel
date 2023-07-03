e7cafe65a135307aadad16979a5ad9bdd05bb95d
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-07-03 15:21:30 +0300

    react-hot-toast install

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Montserrat } from "next/font/google";
+import { Toaster } from "react-hot-toast";
 
 import RootContent from "./components/RootContent";
 import "./globals.css";
@@ -18,6 +19,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={font.className}>
+				<Toaster />
 				<RootContent>{children}</RootContent>
 			</body>
 		</html>
